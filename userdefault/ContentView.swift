e4b3fb1a3a6bd5dd6eeb3c86816c2369f0a11d77
joferava <jfelipe221095@gmail.com>
2021-03-23 21:55:44 -0600

    Initial Commit

---
+++ userdefault/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  userdefault
+//
+//  Created by Felipe Ramirez Vargas on 23/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ userdefault/userdefaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  userdefaultApp.swift
+//  userdefault
+//
+//  Created by Felipe Ramirez Vargas on 23/3/21.
+//
+
+import SwiftUI
+
+@main
+struct userdefaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
